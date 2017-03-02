71f002d9b187de832ef2588608b18026d468037a
Joseph Hellerstein <jlheller@uw.edu>
2017-03-01 18:15:22 -0800

    Preliminary analysis

--- Analysis/data_plotter.py
+++ Analysis/data_plotter.py
@@ -22,11 +22,14 @@ class DataPlotter(object):
     """
     self._provider = data_provider
 
-  def lines(self, names):
+  def lines(self, names, yrange=None):
     """
     Does a stacked line plot with a common time axis.
     """
     num_plots = len(names)
+    xvals = self._provider.getVariable(TIME_VAR)
+    xmin = min(xvals)
+    xmax = max(xvals)
     for idx in range(num_plots):
       plt.subplot(num_plots, 1, idx+1)
       frame = plt.gca()
@@ -35,6 +38,12 @@ class DataPlotter(object):
       else:
         plt.xlabel(TIME_VAR)
       plt.ylabel(names[idx])
-      xvals = self._provider.getVariable(TIME_VAR)
       yvals = self._provider.getVariable(names[idx])
+      if yrange is None:
+        ymin = min(yvals)*0.95
+        ymax = max(yvals)*1.05
+      else:
+        ymin = yrange[0]
+        ymax = yrange[1]
+      plt.axis([xmin, xmax, ymin, ymax])
       plt.plot(xvals, yvals)
