829356e908d532e5ae26d2b71526d4d969710f16
Joseph Hellerstein <jlheller@uw.edu>
2017-03-13 16:28:23 -0700

    Updated states to include is_boundR.

--- Analysis/chemotaxis_model.py
+++ Analysis/chemotaxis_model.py
@@ -20,17 +20,20 @@ The classes are:
     ligand binding, phosphorylation, and methylation
  State represents a single element of the state of receptors
 """
-
+import sys
+sys.path.append("../SbStar/Code")
 import tellurium as te
+from sbstar import SbStar
 
 SIM_START = 0  # Simulation start time
 SIM_END = 500  # Simulation end time
 SIM_SAMPLES_PER_TIME = 10
 TIME = "time"
+TEMPLATE = "chemotaxis.tmpl"
 # Concentration constants
 B_conc = "1.7e-6"
 R_conc = "0.75e-6"  # Differs from Spiro's 0.3e-6
-T_conc = "8e-6"
+T2_conc = "8e-6"
 Y_conc = "20e-6"
 Z_conc = "40e-6"
 
@@ -38,133 +41,43 @@ Z_conc = "40e-6"
 
 class ChemotaxisModel(object):
 
-  MODEL_START = "      model chemotaxis\n"
-  MODEL_END = "      end\n"
-  MODEL = '''
-        J0: $x0 -> L; k0*x0
-        # REACTIONS from Spiro, Table 3
-        # Methylation
-        J1: T2R -> T3 + R; k1c*T2R
-        J2: T3R -> T4 + R; k2c*T3R
-        J3: LT2R -> LT3 + R; k3c*LT2R
-        J4: LT3R -> LT4 + R; k4c*LT3R
-        #   Details of Tn + R <-> TnR
-        J5: T2R -> T2 + R; k1a*T2R
-        J6: T2 + R -> T2R; k1b*T2*R
-        J7: T3R -> T3 + R; k2a*T3R
-        J8: T3 + R -> T3R; k2b*T3*R
-        #J9: T4R -> T4 + R; k3a*T4R
-        #J10: T4 + R -> T4R; k3b*T4*R
-        #   Details of LTn + R <-> LTnR (not in Spiro)
-        J11: LT2R -> LT2 + R; k1a*LT2R
-        J12: LT2 + R -> LT2R; k1b*LT2*R
-        J13: LT3R -> LT3 + R; k2a*LT3R
-        J14: LT3 + R -> LT3R; k2b*LT3*R
-        #J15: LT4R -> LT4 + R; k3a*LT4R
-        #J16: LT4 + R -> LT4R; k3b*LT4*R
-        # Demethylation reactions
-        J17: T3 + Bp -> T2 + Bp; k_1*T3*Bp
-        J18: T4 + Bp -> T3 + Bp; k_2*T4*Bp  
-        J19: LT3 + Bp -> LT2 + Bp; k_3*LT3*Bp
-        J20: LT4 + Bp -> LT3 + Bp; k_4*LT4*Bp
-        # Ligand binding gives details to L + T -> LT, LT -> L + T
-        J21: L + T2 -> LT2; k5*T2*L
-        J22: L + T3 -> LT3; k5*T3*L
-        J23: L + T4 -> LT4; k5*T4*L
-        J24: LT2 -> T2 + L; k_5*LT2
-        J25: LT3 -> T3 + L; k_5*LT3
-        J26: LT4 -> T4 + L; k_5*LT4
-        # Autophosphorylation reactions
-        J27: T2 -> T2p; k8*T2
-        J28: T3 -> T3p; k9*T3
-        J29: T4 -> T4p; k10*T4
-        J30: LT2 -> LT2p; k11*LT2
-        J31: LT3 -> LT3p; k12*LT3
-        J32: LT4 -> LT4p; k13*LT4
-        # B phosphorylations
-        J33:T2p + B -> T2 + Bp; kb*T2p*B
-        J34:T3p + B -> T3 + Bp; kb*T3p*B
-        J35:T4p + B -> T4 + Bp; kb*T4p*B
-        J36:LT2p + B -> LT2 + Bp; kb*LT2p*B
-        J37:LT3p + B -> LT3 + Bp; kb*LT3p*B
-        J38:LT4p + B -> LT4 + Bp; kb*LT4p*B
-        # Y phosphorylations
-        J39:T2p + Y -> T2 + Yp; ky*T2p*Y
-        J40:T3p + Y -> T3 + Yp; ky*T3p*Y
-        J41:T4p + Y -> T4 + Yp; ky*T4p*Y
-        J42:LT2p + Y -> LT2 + Yp; ky*LT2p*Y
-        J43:LT3p + Y -> LT3 + Yp; ky*LT3p*Y
-        J44:LT4p + Y -> LT4 + Yp; ky*LT4p*Y
-        # B & Y dephosphorylations
-        J45: Bp -> B; k_b*Bp
-        J46: Yp + Z -> Y + Z; k_y*Yp*Z    
-        # CONSTANTS from Spiro, Table 3, except k1a (which is noted above).
-        k0 = 0
-        ktuning = 10
-        k1b = ktuning*k5
-        k1a = (1.7e-6)/k1b
-        k2a = k1a
-        k2b = k1b
-        k3a = k1a
-        k3b = k1b
-        k1c = 0.17
-        k2c = 0.1*k1c
-        k3c = 30*k1c
-        k4c = k3c
-        k5  = 7e7
-        k_5 = 70
-        k_1 = 4e5
-        k_2 = 3e4
-        k_3 = k_1
-        k_4 = k_2
-        k8 = 15
-        k9 = 3*k8
-        k10 = 3.2*k8
-        k11 = 0
-        k12 = 1.1*k8
-        k13 = 0.72*k10
-        kb = 8e5
-        ky = 3e7
-        k_b = 0.35
-        k_y = 5e5
-        # INITIAL VALUES from Spiro, Table 2
-        B = %s
-        Bp = 0
-        LT2 = 0
-        LT2R = 0
-        LT3 = 0
-        LT3R = 0
-        LT4 = 0
-        T2p = 0
-        T3p = 0
-        T4p = 0
-        LT2p = 0
-        LT3p = 0
-        LT4p = 0
-        R = %s
-        T2 = %s
-        T3 = 0
-        T4 = 0
-        T2R = 0
-        T3R = 0
-        x0 = 1
-        Y = %s
-        Yp = 0
-        Z = %s
-  ''' % (B_conc, R_conc, T_conc, Y_conc, Z_conc)
+  MODEL_START = "model chemotaxis\n"
+  MODEL_END = "end\n"
 
-  def __init__(self):
+  def __init__(self, template=TEMPLATE):
     self._rr = None
-    self._model = ChemotaxisModel.MODEL
+    self._template = template
+    self._template_model = None
+    self._antimony_model = None
+    self._setupModel()
     self._result = None
     self._factory = None  # State aggregation factory
+
+  def _setupModel(self):
+    """
+    Processes the template model
+    reads: _template
+    writes: _template_model, _antimony_model
+    """
+    lines = []
+    with open(self._template) as file:  
+      lines.append(file.read())
+    self._template_model = "\n".join(lines)
+    names = ['B', 'R', 'T2', 'Y', 'Z']
+    values = [B_conc, R_conc, T2_conc, Y_conc, Z_conc]
+    pairs = zip(names, values)
+    suffixes = ["%s = %s" % (n, v) for n, v in pairs]
+    for suffix in suffixes:
+      self._template_model += suffix + "\n"
+    sbstar = SbStar(self._template_model)
+    self._antimony_model = sbstar.expand()
 
   def _assembleModel(self):
     """
     Assembles the final the model
     """
     return "%s\n%s\n%s" % (ChemotaxisModel.MODEL_START, 
-        self._model, ChemotaxisModel.MODEL_END)
+        self._antimony_model, ChemotaxisModel.MODEL_END)
 
   def initialize(self):
     self._rr = te.loada(self._assembleModel())
@@ -193,10 +106,13 @@ class ChemotaxisModel(object):
     """
     Adds the string to the end of the model
     """
-    self._model = "%s\n%s" % (self._model, stg)
+    self._antimony_model = "%s\n%s" % (self._antimony_model, stg)
 
-  def getModel(self):
-    return self._model
+  def getModel(self, is_template):
+    if is_template:
+      return self._template_model
+    else:
+      return self._antimony_model
 
   def getReceptorStates(self):
     return ReceptorStates(self._result)
@@ -257,28 +173,32 @@ class State(object):
   Represents a single element of the Receptor state.
   """
 
-  def __init__(self, is_ligand, is_phosphorylated, methylation,
-      simulation_result):
+  def __init__(self, is_boundL, is_phosphorylated, is_boundR, 
+      methylation, simulation_result):
     """
-    :param bool is_ligand: bound to a ligand
+    :param bool is_boundL: bound to a ligand
     :param bool is_phosphorylated: is phosphorylated
+    :param bool is_boundR: bound to a CheR
     :param int methylation: methylation level
     :param StructuredArray simulation_result:
     """
-    self.is_ligand = is_ligand
+    self.is_boundL = is_boundL
     self.is_phosphorylated = is_phosphorylated
+    self.is_boundR = is_boundR
     self.methylation = methylation
     self._name = self._makeName()
     self._data = self._extractData(simulation_result)
 
   def _makeName(self):
-    if self.is_ligand:
+    if self.is_boundL:
       name = "LT"
     else:
       name = "T"
     name = "%s%d" % (name, self.methylation)
     if self.is_phosphorylated:
       name = name + "p"
+    if self.is_boundR:
+      name = name + "R"
     #return "[" + name + "]"
     return name
 
@@ -319,31 +239,33 @@ class ReceptorStates(object):
     self._makeStates()
 
   def _makeStates(self):
-    is_ligands = [False, True]
+    is_boundLs = [False, True]
     is_phosphorylateds = [False, True]
+    is_boundRs = [False, True]
     methylations = [2, 3, 4]
     states = []
-    for l in is_ligands:
+    for l in is_boundLs:
       for p in is_phosphorylateds:
-        for m in methylations:
-          states.append(State(l, p, m, self._result))
+        for r in is_boundRs:
+          for m in methylations:
+            states.append(State(l, p, r, m, self._result))
     self._states = states
-    total = self.sumStates(lambda l,m,p: True)
+    total = self.sumStates(lambda l,m,r,p: True)
     [s.setNominalValue(total) for s in states]
 
   def selectStates(self, func):
     """
     Returns a list of states for which func is true
-    :param bool-Function func: arguments are is_ligand, is_phosphorylated, methylation
+    :param bool-Function func: arguments are is_boundL, is_phosphorylated, methylation
     :return list-of-State: those states for which func returns True
     """
     return [s for s in self._states 
-            if func(s.is_ligand, s.is_phosphorylated, s.methylation)]
+            if func(s.is_boundL, s.is_phosphorylated, s.is_boundR, s.methylation)]
 
   def sumStates(self, func):
     """
     Returns a the sum of values of selected states
-    :param bool-Function func: arguments are is_ligand, is_phosphorylated, methylation
+    :param bool-Function func: arguments are is_boundL, is_phosphorylated, methylation
     :return ndarray:
     """
     data = [s.getData() for s in self.selectStates(func)]
@@ -352,7 +274,7 @@ class ReceptorStates(object):
   def frcStates(self, func):
     """
     Returns the fraction of time in the specified states
-    :param bool-Function func: arguments are is_ligand, is_phosphorylated, methylation
+    :param bool-Function func: arguments are is_boundL, is_phosphorylated, methylation
     :return ndarray:
     """
     data = [s.getNominalData() for s in self.selectStates(func)]
@@ -386,6 +308,7 @@ class StateAggregationFactory(object):
     :raises ValueError: invalid character
     """
     letter = name[position]
+    is_valid = False
     if letter ==  "_":
       func = (lambda x: True)
       is_valid = True
@@ -394,14 +317,15 @@ class StateAggregationFactory(object):
         is_valid = validation_function(letter)
       except Exception:
         is_valid = False
-      if is_valid and letter == "T":
-        func = (lambda x: x)
-      elif is_valid and letter == "F":
-        func = (lambda x: not x)
-      elif is_valid and isinstance(int(letter), int):
-        func = eval("(lambda x: x == %s)" % letter)
-      else:
-        is_valid = False
+      if is_valid:
+        if letter == "T":
+          func = (lambda x: x)
+        elif letter == "F":
+          func = (lambda x: not x)
+        elif isinstance(int(letter), int):
+          func = eval("(lambda x: x == %s)" % letter)
+        else:
+          is_valid = False
     if not is_valid:
       raise ValueError("In name %s, the character %s in position %d is invalid."
           % (name, letter, position))
@@ -410,18 +334,18 @@ class StateAggregationFactory(object):
   def v(self, name):
     """
     Returns the values for the name
-    :param list-of-4-char name:
+    :param list-of-5-char name:
     """
     # Construct the globals algorithmically using eval
-    if len(name) != 4:
-      raise ValueError("Name must be 4 characters.")
-    lfunc = self._getFunc(name, 1,
-        lambda x: x in ["T", "F"])
-    pfunc = self._getFunc(name, 2,
-        lambda x: x in ["T", "F"])
-    mfunc = self._getFunc(name, 3, 
+    valid_boolean_function = lambda x: x in ["T", "F"]
+    if len(name) != 5:
+      raise ValueError("Name must be 5 characters.")
+    lfunc = self._getFunc(name, 1, valid_boolean_function)
+    pfunc = self._getFunc(name, 2, valid_boolean_function)
+    rfunc = self._getFunc(name, 3, valid_boolean_function)
+    mfunc = self._getFunc(name, 4, 
         lambda x: isinstance(int(x),int) and int(x) > 1 and int(x) < 5)
-    func = (lambda l,p,m: lfunc(l) and pfunc(p) and mfunc(m))
+    func = (lambda l,p,r,m: lfunc(l) and pfunc(p) and rfunc(p) and mfunc(m))
     if name[0] == "t":
       result = self._receptor_states.sumStates(func)
     else:
--- Analysis/test_chemotaxis_model.py
+++ Analysis/test_chemotaxis_model.py
@@ -1,9 +1,12 @@
 import unittest
 from chemotaxis_model import ChemotaxisModel, ReceptorStates, State,  \
-    T_conc, StateAggregationFactory, R_conc, B_conc, Y_conc
+    T2_conc, StateAggregationFactory, R_conc, B_conc, Y_conc
 
 
 IGNORE_TEST = False
+NUM_METHYL = 3
+# States are: is_boundL (T/F), is_phosphorylated (T/F), is_boundR (T/F)
+NUM_STATES = 2*2*2*NUM_METHYL
 
 
 #############################
@@ -21,19 +24,18 @@ class TestChemotaxisModelBasic(unittest.TestCase):
   def testConstructor(self):
     if IGNORE_TEST:
       return
-    self.assertTrue('Yp =' in self.model._model)
+    self.assertTrue('Yp =' in self.model._antimony_model)
 
   def testInitialize(self):
     if IGNORE_TEST:
       return
     rr = self.model.initialize()
-    self.assertEqual(rr.R, 0.3e-6)
+    self.assertTrue(abs(self.model._rr.R - float(R_conc)) <= 1e-9)
 
   def testRun(self):
     if IGNORE_TEST:
       return
     self.model.initialize()
-    self.assertTrue(abs(self.model._rr.R - 0.3e-6) <= 1e-9)
     self.model.run()
 
   def testGetVariable(self):
@@ -55,15 +57,11 @@ class TestChemotaxisModelBasic(unittest.TestCase):
     b = [y == B_conc for y in B + Bp]
     self.assertTrue(b)
     #
-    total = self.model.getVariable("t___")
-    b = [y == T_conc for y in total]
-    self.assertTrue(b)
+    var = self.model.getVariable("J1_2R")
+    self.assertEqual(len(var), len(Bp))
     #
-    J1 = self.model.getVariable("J1")
-    self.assertEqual(len(J1), len(Bp))
-    #
-    J1 = self.model.getVariable("time")
-    self.assertEqual(len(J1), len(Bp))
+    time = self.model.getVariable("time")
+    self.assertEqual(len(time), len(Bp))
  
     
 
@@ -76,13 +74,13 @@ class TestState(unittest.TestCase):
     self.model.initialize()
     self.model.run(start=0, end=10)
 
-  def _testBasics(self, l, p, m, expected_name):
+  def _testBasics(self, l, p, r, m, expected_name):
     """
     :param bool l: is ligand bound
     :param bool p: is phosphorylated
     :param int m: methylation 
     """
-    state = State(l, p, m, self.model.getResult())
+    state = State(l,p,r, m, self.model.getResult())
     name = state.getName()
     self.assertEqual(name, expected_name)
     pairs = zip(state.getData(), self.model.getResult()[name])
@@ -92,10 +90,10 @@ class TestState(unittest.TestCase):
     self.assertTrue(b)
 
   def testBasics(self):
-    self._testBasics(True, True, 2, 'LT2p')
-    self._testBasics(False, True, 2, 'T2p')
-    self._testBasics(True, False, 2, 'LT2')
-    self._testBasics(True, True, 3, 'LT3p')
+    self._testBasics(True, True, False, 2, 'LT2p')
+    self._testBasics(False, True, False, 2, 'T2p')
+    self._testBasics(True, False, False, 2, 'LT2')
+    self._testBasics(True, True, True, 3, 'LT3pR')
 
 
 # pylint: disable=W0212,C0111,R0904
@@ -108,10 +106,10 @@ class TestReceptorStates(unittest.TestCase):
 
   def testBasic(self):
     receptor = ReceptorStates(self.model.getResult())
-    self.assertEqual(len(receptor._states), 12)
+    self.assertEqual(len(receptor._states), NUM_STATES)
     # Select a state that should have no occupancy
     state = [s for s in receptor._states if
-                s.is_ligand and (s.methylation == 2) and
+                s.is_boundL and (s.methylation == 2) and
                 s.is_phosphorylated][0]
     b = all([x < 0.01 for x in state.getNominalData()])
     self.assertTrue(b)
@@ -122,26 +120,26 @@ class TestReceptorStates(unittest.TestCase):
     self.assertEqual(len(states), expected)
 
   def testSelectStates(self):
-    func = (lambda l,p,m: l)
-    self._testSelectStates(func, 6)
-    func = (lambda l,p,m: p)
-    self._testSelectStates(func, 6)
-    func = (lambda l,p,m: m == 2)
-    self._testSelectStates(func, 4)
+    func = (lambda l,p,r,m: l)
+    self._testSelectStates(func, NUM_STATES/2)
+    func = (lambda l,p,r,m: p)
+    self._testSelectStates(func, NUM_STATES/2)
+    func = (lambda l,p,r,m: m == 2)
+    self._testSelectStates(func, NUM_STATES/NUM_METHYL)
 
   # TODO: Losing about 3% of the receptors
   def testSumStates(self):
     # Test that the sum of all states results in the Receptor conc
-    func = (lambda l,p,m: True)
+    func = (lambda l,p,r,m: True)
     receptor = ReceptorStates(self.model.getResult())
     total = receptor.sumStates(func)
-    T = float(T_conc)
-    b = all([abs(T-x)/T < 0.04 for x in total])
+    T = float(T2_conc)
+    b = all([abs(T-x)/T < 0.01 for x in total])
     self.assertTrue(b)
 
   def testFrcStates(self):
     # Test that the sum of all states results in the Receptor conc
-    func = (lambda l,p,m: True)
+    func = (lambda l,p,r,m: True)
     receptor = ReceptorStates(self.model.getResult())
     total = receptor.frcStates(func)
     b = all([abs(1-x) < 0.01 for x in total])
@@ -171,13 +169,13 @@ class TestStateAggregationFactory(unittest.TestCase):
     self.assertFalse(func(3))
 
   def testV(self):
-    value = self.factory.v("t___")
-    expected = self.receptor.sumStates(lambda l,p,m: True)
+    value = self.factory.v("t____")
+    expected = self.receptor.sumStates(lambda l,p,r,m: True)
     pairs = zip(value, expected)
     b = all([abs(x-y) < abs(y)*0.001 for x,y in pairs])
     self.assertTrue(b)
     count = len(expected)
-    names = ["_T3", "T_3", "TT_", "T__", "_T_", "__3", "___"]
+    names = ["_T_3", "T__3", "TT__", "T___", "_T__", "___3", "____"]
     for name in names:
       try:
         variable = "f%s" % name
